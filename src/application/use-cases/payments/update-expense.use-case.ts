de67720df69cdc40ddbdf1a263af932c893407d4
Jerome Agapay <jerome.arceo.agapay@gmail.com>
2025-12-02 13:38:56 +0800

    fix updated repository

--- src/application/use-cases/payments/update-expense.use-case.ts
+++ src/application/use-cases/payments/update-expense.use-case.ts
@@ -1,9 +1,9 @@
-import { UpdateExpenseInputSchema } from "src/entities/models/Payment";
-import { PaymentRepository } from "src/infrastructure/repositories/payments.repository";
+import { UpdateExpenseInputSchema } from "src/entities/models/Expense";
+import { ExpensesRepository } from "src/infrastructure/repositories/expenses.repository";
 
 export const updateExpenseUseCase = async (
   expense_id: string,
   data: UpdateExpenseInputSchema
 ) => {
-  return PaymentRepository.updateExpense(expense_id, data);
+  return ExpensesRepository.updateExpense(expense_id, data);
 };
