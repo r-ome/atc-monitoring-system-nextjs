390151f614a5b763e7c3c9e808df1bb0b0b4d0ab
Jerome Agapay <jerome.arceo.agapay@gmail.com>
2026-01-14 09:57:46 +0800

    fix: fixed register bidder modal to reset after every registration

--- app/(protected)/auctions/components/RegisterBidderModal.tsx
+++ app/(protected)/auctions/components/RegisterBidderModal.tsx
@@ -40,6 +40,7 @@ import { Auction } from "src/entities/models/Auction";
 import { RegisteredBidder } from "src/entities/models/Bidder";
 import { toast } from "sonner";
 import { PaymentMethod } from "src/entities/models/PaymentMethod";
+import { ScrollArea } from "@/app/components/ui/scroll-area";
 
 interface RegisterBidderModalProps {
   auction: Auction;
@@ -105,6 +106,10 @@ export const RegisterBidderModal: React.FC<RegisterBidderModalProps> = ({
     fetchInitialData();
   }, []);
 
+  useEffect(() => {
+    if (!open) setSelectedBidder(undefined);
+  }, [open]);
+
   const filteredBidders = useMemo(() => {
     const registered = new Set(
       registeredBidders.map((item) => item.bidder.bidder_id)
@@ -154,6 +159,7 @@ export const RegisterBidderModal: React.FC<RegisterBidderModalProps> = ({
     if (res) {
       setIsLoading(false);
       if (res.ok) {
+        setSelectedBidder(undefined);
         toast.success("Successfully Registered Bidder");
         router.refresh();
         setOpen(false);
@@ -167,254 +173,202 @@ export const RegisterBidderModal: React.FC<RegisterBidderModalProps> = ({
     }
   };
 
-  // const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-  //   setIsLoading(true);
-  //   // bidder_numbers
-  //   const bidder_numbers = [
-
-  //   ];
-  //   const bidder_with_details = bidder_numbers
-  //     .filter((item) =>
-  //       filteredBidders.find((bidder) => bidder.bidder_number === item)
-  //     )
-  //     .map((item) => {
-  //       const match = filteredBidders.find(
-  //         (bidder) => bidder.bidder_number === item
-  //       );
-
-  //       return {
-  //         bidder_id: match?.bidder_id,
-  //         bidder_number: match?.bidder_number,
-  //         registration_fee: match?.registration_fee,
-  //         service_charge: match?.service_charge,
-  //       };
-  //     });
-
-  //   await Promise.all(
-  //     bidder_with_details.map((item) => {
-  //       const formData = new FormData();
-  //       formData.append("auction_id", auction.auction_id);
-  //       formData.append("bidder_id", item.bidder_id as string);
-  //       formData.append(
-  //         "registration_fee",
-  //         item.registration_fee?.toString() as string
-  //       );
-  //       formData.append(
-  //         "service_charge",
-  //         item.service_charge?.toString() as string
-  //       );
-  //       const balance = (item.registration_fee as number) * -1;
-  //       formData.append("balance", balance.toString());
-  //       formData.append("payment_method", "CASH");
-  //       return registerBidder(formData);
-  //     })
-  //   );
-
-  //   toast.success("Done!");
-  //   router.refresh();
-  //   setOpen(false);
-  //   setIsLoading(false);
-  // };
-
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <Button>Register Bidder</Button>
       </DialogTrigger>
-      <DialogContent>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <DialogHeader>
-            <DialogTitle>Register Bidder</DialogTitle>
-            <DialogDescription>
-              Please select a bidder to register
-            </DialogDescription>
-          </DialogHeader>
+      <DialogContent className="min-w-[700px]">
+        <div className="min-w-[400px]">
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <DialogHeader>
+              <DialogTitle>Register Bidder</DialogTitle>
+              <DialogDescription>
+                Please select a bidder to register
+              </DialogDescription>
+            </DialogHeader>
 
-          <SelectWithSearch
-            modal={true}
-            side="bottom"
-            placeholder="Search Bidder to register"
-            setSelected={(selected) => setSelectedBidder(selected)}
-            options={filteredBidders.map((bidder) => ({
-              label: `${bidder.full_name} (${bidder.bidder_number}) ${
-                bidder.status === "BANNED" ? bidder.status : ""
-              }`,
-              value: bidder.bidder_id,
-              disabled: bidder.status === "BANNED",
-              registration_fee: bidder.registration_fee,
-              service_charge: bidder.service_charge,
-              payment_term: bidder.payment_term,
-            }))}
-          />
-          <div className="flex gap-4">
-            <div className="flex flex-col gap-2">
-              <Label>Service Charge:</Label>
-              <InputNumber
-                id="service_charge"
-                name="service_charge"
-                defaultValue={12}
-                value={selectedBidder?.service_charge as number}
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Label>Registration Fee:</Label>
-
-              <InputNumber
-                id="registration_fee"
-                name="registration_fee"
-                defaultValue={3000}
-                value={selectedBidder?.registration_fee as number}
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Label>Payment Term:</Label>
-
-              <InputNumber
-                id="payment_term"
-                name="payment_term"
-                disabled
-                hasStepper={false}
-                suffix=" days"
-                value={selectedBidder?.payment_term as number}
-                required
-              />
-            </div>
-          </div>
-
-          <div>
-            <div className="flex flex-col gap-2">
-              <Tabs
-                defaultValue="single"
-                onValueChange={() => setPayments([...initialState])}
-              >
-                <TabsList className="w-full">
-                  <TabsTrigger value="single">Single/Full Payment</TabsTrigger>
-                  <TabsTrigger value="multiple">Multiple Payments</TabsTrigger>
-                </TabsList>
-
-                <TabsContent value="single">
-                  <Select
-                    required
-                    onValueChange={(value) => handleMethodChange(0, value)}
-                  >
-                    <SelectTrigger className="w-full">
-                      <SelectValue placeholder="Select Payment Type"></SelectValue>
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        {paymentMethods.map((item) => (
-                          <SelectItem
-                            key={item.payment_method_id}
-                            value={item.payment_method_id}
-                          >
-                            {item.name}
-                          </SelectItem>
-                        ))}
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-
-                  {payments.length ? (
-                    <input
-                      type="hidden"
-                      value={0}
-                      name={`PAYMENT_${payments[0].method}`}
+            <SelectWithSearch
+              modal={true}
+              side="bottom"
+              placeholder="Search Bidder to register"
+              setSelected={(selected) => setSelectedBidder(selected)}
+              options={filteredBidders.map((bidder) => ({
+                label: `${bidder.full_name} (${bidder.bidder_number}) ${
+                  bidder.status === "BANNED" ? bidder.status : ""
+                }`,
+                value: bidder.bidder_id,
+                disabled: bidder.status === "BANNED",
+                registration_fee: bidder.registration_fee,
+                service_charge: bidder.service_charge,
+                payment_term: bidder.payment_term,
+              }))}
+            />
+            {selectedBidder === undefined ? null : (
+              <>
+                <div className="flex gap-4">
+                  <div className="flex flex-col gap-2">
+                    <Label>Service Charge:</Label>
+                    <InputNumber
+                      id="service_charge"
+                      name="service_charge"
+                      defaultValue={12}
+                      value={selectedBidder?.service_charge as number}
                     />
-                  ) : null}
-                </TabsContent>
-
-                <TabsContent value="multiple">
-                  <div className="space-y-2">
-                    {payments.map((item, index) => (
-                      <div className="flex gap-2" key={index}>
-                        <div className="flex-1">
-                          <Select
-                            required
-                            value={payments[index].method}
-                            onValueChange={(value) =>
-                              handleMethodChange(index, value)
-                            }
-                          >
-                            <SelectTrigger className="w-full">
-                              <SelectValue placeholder="Select Payment Type"></SelectValue>
-                            </SelectTrigger>
-                            <SelectContent>
-                              <SelectGroup>
-                                {paymentMethods.map((item) => (
-                                  <SelectItem
-                                    key={item.payment_method_id}
-                                    value={item.payment_method_id}
-                                  >
-                                    {item.name}
-                                  </SelectItem>
-                                ))}
-                              </SelectGroup>
-                            </SelectContent>
-                          </Select>
-                        </div>
-
-                        <div className="flex-1">
-                          <InputNumber
-                            name={`PAYMENT_${item.method}_${index}`}
-                            required
-                            onChange={(e) =>
-                              handleAmountChange(index, Number(e.target.value))
-                            }
-                          />
-                        </div>
-
-                        <Button
-                          variant="destructive"
-                          onClick={() => handleRemove(index)}
-                        >
-                          <CircleX />
-                        </Button>
-                      </div>
-                    ))}
-
-                    {payments.length < paymentMethods.length && (
-                      <Button
-                        type="button"
-                        variant="outline"
-                        onClick={handleAdd}
-                        disabled={payments.some((item) => item.method === "")}
-                      >
-                        Add Payment Method
-                      </Button>
-                    )}
                   </div>
-                </TabsContent>
-              </Tabs>
-              {/* <Label>Payment Method:</Label>
+                  <div className="flex flex-col gap-2">
+                    <Label>Registration Fee:</Label>
 
-              <SelectWithSearch
-                modal={true}
-                side="bottom"
-                defaultValue={
-                  selectedPaymentMethod as { label: string; value: string }
-                }
-                placeholder="Choose Payment Method"
-                setSelected={(selected) =>
-                  setSelectedPaymentMethod(selected as Record<string, string>)
-                }
-                options={[
-                  { label: "BDO", value: "BDO" },
-                  { label: "BPI", value: "BPI" },
-                  { label: "CASH", value: "CASH" },
-                  { label: "GCASH", value: "GCASH" },
-                ]}
-              /> */}
-            </div>
-          </div>
-          <DialogFooter>
-            <DialogClose className="cursor-pointer">Cancel</DialogClose>
-            <Button type="submit" disabled={isLoading || !selectedBidder}>
-              {isLoading && <Loader2Icon className="animate-spin" />}
-              Submit
-            </Button>
-          </DialogFooter>
-        </form>
+                    <InputNumber
+                      id="registration_fee"
+                      name="registration_fee"
+                      defaultValue={3000}
+                      value={selectedBidder?.registration_fee as number}
+                    />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <Label>Payment Term:</Label>
+
+                    <InputNumber
+                      id="payment_term"
+                      name="payment_term"
+                      disabled
+                      hasStepper={false}
+                      suffix=" days"
+                      value={selectedBidder?.payment_term as number}
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <div className="flex flex-col gap-2">
+                    <Tabs
+                      defaultValue="single"
+                      onValueChange={() => setPayments([...initialState])}
+                    >
+                      <TabsList className="w-full">
+                        <TabsTrigger value="single">
+                          Single/Full Payment
+                        </TabsTrigger>
+                        <TabsTrigger value="multiple">
+                          Multiple Payments
+                        </TabsTrigger>
+                      </TabsList>
+
+                      <TabsContent value="single">
+                        <Select
+                          required
+                          onValueChange={(value) =>
+                            handleMethodChange(0, value)
+                          }
+                        >
+                          <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Select Payment Type"></SelectValue>
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectGroup>
+                              {paymentMethods.map((item) => (
+                                <SelectItem
+                                  key={item.payment_method_id}
+                                  value={item.payment_method_id}
+                                >
+                                  {item.name}
+                                </SelectItem>
+                              ))}
+                            </SelectGroup>
+                          </SelectContent>
+                        </Select>
+
+                        {payments.length ? (
+                          <input
+                            type="hidden"
+                            value={0}
+                            name={`PAYMENT_${payments[0].method}`}
+                          />
+                        ) : null}
+                      </TabsContent>
+
+                      <TabsContent value="multiple">
+                        <div className="space-y-2">
+                          {payments.map((item, index) => (
+                            <div className="flex gap-2" key={index}>
+                              <div className="flex-1">
+                                <Select
+                                  required
+                                  value={payments[index].method}
+                                  onValueChange={(value) =>
+                                    handleMethodChange(index, value)
+                                  }
+                                >
+                                  <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Select Payment Type"></SelectValue>
+                                  </SelectTrigger>
+                                  <SelectContent>
+                                    <SelectGroup>
+                                      {paymentMethods.map((item) => (
+                                        <SelectItem
+                                          key={item.payment_method_id}
+                                          value={item.payment_method_id}
+                                        >
+                                          {item.name}
+                                        </SelectItem>
+                                      ))}
+                                    </SelectGroup>
+                                  </SelectContent>
+                                </Select>
+                              </div>
+
+                              <div className="flex-1">
+                                <InputNumber
+                                  name={`PAYMENT_${item.method}_${index}`}
+                                  required
+                                  onChange={(e) =>
+                                    handleAmountChange(
+                                      index,
+                                      Number(e.target.value)
+                                    )
+                                  }
+                                />
+                              </div>
+
+                              <Button
+                                variant="destructive"
+                                onClick={() => handleRemove(index)}
+                              >
+                                <CircleX />
+                              </Button>
+                            </div>
+                          ))}
+
+                          {payments.length < paymentMethods.length && (
+                            <Button
+                              type="button"
+                              variant="outline"
+                              onClick={handleAdd}
+                              disabled={payments.some(
+                                (item) => item.method === ""
+                              )}
+                            >
+                              Add Payment Method
+                            </Button>
+                          )}
+                        </div>
+                      </TabsContent>
+                    </Tabs>
+                  </div>
+                </div>
+              </>
+            )}
+            <DialogFooter>
+              <DialogClose className="cursor-pointer">Cancel</DialogClose>
+              <Button type="submit" disabled={isLoading || !selectedBidder}>
+                {isLoading && <Loader2Icon className="animate-spin" />}
+                Submit
+              </Button>
+            </DialogFooter>
+          </form>
+        </div>
       </DialogContent>
     </Dialog>
   );
