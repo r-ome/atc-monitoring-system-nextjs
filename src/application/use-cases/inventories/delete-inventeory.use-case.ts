a77c7d85059c23da1bade722cda44f54e60aedfc
Jerome Agapay <jerome.arceo.agapay@gmail.com>
2026-02-21 17:48:20 +0800

    fix: fixed upload error on duplicate manifest

--- src/application/use-cases/inventories/delete-inventeory.use-case.ts
+++
@@ -1,5 +0,0 @@
-import { InventoryRepository } from "src/infrastructure/repositories/inventories.repository";
-
-export const deleteInventoryUseCase = async (inventory_id: string) => {
-  return InventoryRepository.deleteInventory(inventory_id);
-};
